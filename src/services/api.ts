0f0e2529b58ca441424b5e08b8993a3e135acbe9
Dillon Erb <dillon@paperspace.com>
2021-09-21 16:50:17 -0400

    trailing slash for static export

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-export const baseURL = process.env.NEXT_PUBLIC_API_URL!
+export const baseURL = ""
 
 export const API = axios.create({ baseURL })
